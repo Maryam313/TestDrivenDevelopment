9b6a49dc22b15445ef8caa32e5a90a3293dd9807
Maryam313 <119934446+Maryam313@users.noreply.github.com>
2023-01-11 08:26:43 +0400

    Done!

---
+++ src/test/java/TDD2/MatchPairTest.java
@@ -0,0 +1,43 @@
+package TDD2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MatchPairTest {
+
+	@Test
+	@DisplayName("Take an empty string :")
+	void test() {	
+		String str="";
+		MatchPair pair=new MatchPair();
+		assertEquals(true, pair.BalancedExpression(str));
+	}
+
+	@Test
+	@DisplayName("Take null: ")
+	void Null() {
+		MatchPair pair=new MatchPair();
+		String str=null;
+		assertThrows(NullPointerException.class, () -> pair.BalancedExpression(str));
+
+	}
+
+	@Test
+	@DisplayName("Can throw an exception.: ")
+	 void testBalancedExpression() {
+		MatchPair pair=new MatchPair();
+		try {
+			pair.BalancedExpression("[{(a+b)}]");
+			fail("Expected an IllegalArgumentException to be thrown");
+		} catch (IllegalArgumentException e) {
+			assertEquals("Invalid characters in expression", e.getMessage());
+		}
+	}
+
+
+}
---
+++ src/test/java/TDD2/SplitMethodTest.java
@@ -0,0 +1,46 @@
+package TDD2;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import TDD2.SplitMethod;
+
+class SplitMethodTest {
+
+	@Test
+	@DisplayName("Take null for both of the arguments. ")
+	void spliteNull() {
+	String input=null;
+	ArrayList<String> str = new ArrayList<>();
+	str.add("");
+	SplitMethod sp = new SplitMethod();
+	assertEquals(str,sp.splitMethod(input,null));	
+	}
+
+	@Test
+	@DisplayName("Take empty for both of the arguments. ")
+	void splitEmpty() {
+	SplitMethod sp = new SplitMethod();	
+	char input=' ';
+	ArrayList<String> str = new ArrayList<>();
+	assertEquals(str,sp.splitMethod(" ", input));
+
+	}
+
+	@Test
+	@DisplayName("Take input. ")
+	void split() {
+		SplitMethod sp = new SplitMethod();
+		ArrayList<String> str = new ArrayList<>();
+		String input="Reverse me ";
+		str.add("Reverse");
+		str.add("me");
+	assertEquals(str,sp.splitMethod(input, ' '));
+
+	}
+
+
+}
