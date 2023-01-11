b612b39063e7ffecfe12c472e22cd2d86df8a9ac
Maryam313 <119934446+Maryam313@users.noreply.github.com>
2023-01-11 08:00:53 +0400

    Done!

--- src/test/java/TDD2/MatchPairTest.java
+++ src/test/java/TDD2/MatchPairTest.java
@@ -13,31 +13,31 @@ class MatchPairTest {
 	@Test
 	@DisplayName("Take an empty string :")
 	void test() {	
-	String str="";
-	MatchPair pair=new MatchPair();
-	assertEquals(true, pair.BalancedExpression(str));
+		String str="";
+		MatchPair pair=new MatchPair();
+		assertEquals(true, pair.BalancedExpression(str));
 	}
-	
+
 	@Test
 	@DisplayName("Take null: ")
 	void Null() {
-	MatchPair pair=new MatchPair();
-	String str=null;
-	assertThrows(NullPointerException.class, () -> pair.BalancedExpression(str));
-   
-	}
-	
-	@Test
-	@DisplayName("Can throw an exception.: ")
-	public void testBalancedExpression() {
 		MatchPair pair=new MatchPair();
-	    try {
-	        pair.BalancedExpression("[{(a+b)}]");
-	        fail("Expected an IllegalArgumentException to be thrown");
-	    } catch (IllegalArgumentException e) {
-	        assertEquals("Invalid characters in expression", e.getMessage());
-	    }
+		String str=null;
+		assertThrows(NullPointerException.class, () -> pair.BalancedExpression(str));
+
 	}
 
-	
+	@Test
+	@DisplayName("Can throw an exception.: ")
+	 void testBalancedExpression() {
+		MatchPair pair=new MatchPair();
+		try {
+			pair.BalancedExpression("[{(a+b)}]");
+			fail("Expected an IllegalArgumentException to be thrown");
+		} catch (IllegalArgumentException e) {
+			assertEquals("Invalid characters in expression", e.getMessage());
+		}
+	}
+
+
 }
